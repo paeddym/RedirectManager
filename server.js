caf2d6a166d6f3eae47f54298ea635ecf6d2a2ee
paeddym <patrikmarosvary@gmail.com>
2023-12-15 15:04:07 +0100

    Finished base frame

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const port = 8080;
+const filename = __dirname + "/redirects.json";
+
+app.get();
+
+app.get();
+
+app.delete();
+
+app.post();
